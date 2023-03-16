b9f6735fee1b460d1a25f76acf5c836480ad674b
rikako <jc180215b@gmail.com>
2023-03-16 18:10:57 +0900

    課題１

---
+++ src/service/MemberService.java
@@ -0,0 +1,15 @@
+package service;
+
+import java.util.ArrayList;
+
+import entity.Member;
+
+public interface MemberService {
+
+	String greet(int i);
+	
+	int sumOf(int x, int y);
+
+	ArrayList<Member> getAll();
+
+}
--- src/service/MemberServiceImpl.java
+++ src/service/MemberServiceImpl.java
@@ -24,4 +24,13 @@ public class MemberServiceImpl implements MemberService {
 
 	}
 
+	@Override
+	public int sumOf(int x, int y) {
+		int sum = 0;
+		for(int i = x; i <= y; i++) {
+			sum += i;
+		}
+		return sum;
+	}
+
 }
